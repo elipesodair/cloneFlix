cd4e2cabf296199c74542204a7bdcaeda58a82f7
unknown <elipesodair0512@gmail.com>
2021-12-14 20:56:33 -0300

    three days-version-3

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ export default () => {
 
     {movieList.length <= 0 &&
       <div className="loading">
-        <img src="https://cdn.lowgif.com/small/0534e2a412eeb281-the-counterintuitive-tech-behind-netflix-s-worldwide.gif" alt="loading"></img>
+        <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="loading"></img>
       </div>
     }
 
